1053e11c22d4a0c246aef2f0e441858e3efa7878
Dinesh7571 <cat505600@gmail.com>
2024-09-28 00:14:12 +0530

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 
 require('dotenv').config()
-
+const cors= require('cors')
 const express=require('express')
 const connectDB=require('./config/connect')
 const notificationRoutes=require('./routes/notification')
 
 const app=express()
 app.use(express.json())
-
+app.use(cors())
 app.use('/notification',notificationRoutes )
  app.get("/test",(req,res)=>{
     res.send({msg:"running.."})
